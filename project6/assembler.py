0c478f0ef392e130cd3e597abff5a68f09ad3687
keyxcode <keyxcode@gmail.com>
2024-08-30 17:52:59 +0700

    Create parse function outline

--- project6/assembler.py
+++ project6/assembler.py
@@ -2,17 +2,48 @@ import sys
 from typing import Dict
 
 
-def symbol_table() -> Dict[str, str]:
-    table = dict()
-    return table
+def init_symbol_table() -> Dict[str, int]:
+    symbol_table = {
+        "SP": 0,
+        "LCL": 1,
+        "ARG": 2,
+        "THIS": 3,
+        "THAT": 4,
+        **{("R" + str(i)): i for i in range(16)},
+        "SCREEN": 16384,
+        "KBD": 24576,
+    }
+    return symbol_table
 
 
-def first_pass(filename: str) -> None:
+def parse(in_filename: str, out_filename: str) -> None:
     # go through the entire program line by line
-    # enter (program’s labels : their ROM addresses) to the symbol table
-    with open(filename, "r") as file:
-        for line in file:
-            pass
+    # - preprocess (remove whitespace, ignore empty lines)
+    # - determine if instruction is A/C/L (pseudo label instruction)
+    # - translate A inst to 0address
+    # - translate C inst using the code module
+    # - write each translated line to out
+
+    # enter (program’s labels : their ROM addresses) to the symbol table???
+
+    with open(in_filename, "r") as infile, open(out_filename, "w") as outfile:
+        for line in infile:
+            stripped_line = line.strip()
+            if stripped_line == "":
+                continue
+
+            if stripped_line[0] == "@":  # A instruction
+                out_line = "0" + str(bin(int(stripped_line[1:])))
+            elif stripped_line[0] == "(":  # L pseudo instruction
+                pass  # ignore for now
+            else:  # C instruction
+
+                comp = ""
+                dest = ""
+                jump = ""
+                out_line = "111" + comp + dest + jump
+
+            outfile.write(out_line + "\n")
 
 
 def second_pass(filename: str) -> None:
@@ -29,10 +60,15 @@ def main():
         print("Usage: python assembler.py <filename>")
         sys.exit(1)
 
-    filename = sys.argv[1]
+    in_filename = sys.argv[1]
+    out_filename = in_filename.split(".")[0] + ".hack"
 
-    first_pass(filename)
-    second_pass(filename)
+    # symbol_table = init_symbol_table()
+
+    parse(in_filename, out_filename)
+
+    # first_pass(filename)
+    # second_pass(filename)
 
 
 if __name__ == "__main__":
