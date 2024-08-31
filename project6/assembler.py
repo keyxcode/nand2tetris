98c77b3beb1e6d0be5af20002609fa54f83bb1eb
keyxcode <keyxcode@gmail.com>
2024-08-31 19:47:48 +0700

    Refactor parse function to Assembler class

--- project6/assembler.py
+++ project6/assembler.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Dict, Optional
+from typing import Dict, List, TextIO
 
 
 def to_binary(value: int, bits: int = 15) -> str:
@@ -70,7 +70,7 @@ def read_label_symbols(filename: str, symbol_table: Dict[str, str]) -> None:
 
 class Code:
     """
-    A class to handle the translation of Hack assembly C instruction fields to binary code.
+    A class to handle the translation of Hack assembly C-instruction fields to binary code.
 
     Attributes:
         comp_dict (dict): A dictionary mapping computation mnemonics to their binary codes.
@@ -138,10 +138,10 @@ class Code:
 
     def translate(self, inst: str, field: int) -> str:
         """
-        Translates a Hack assembly C instruction field to its binary representation.
+        Translates a Hack assembly C-instruction field to its binary representation.
 
         Args:
-            inst (str): The C instruction field (e.g., "D", "M", "JEQ").
+            inst (str): The C-instruction field (e.g., "D", "M", "JEQ").
             field (int): The type of field to translate:
                 - 0: comp
                 - 1: dest
@@ -164,63 +164,119 @@ class Code:
             raise KeyError("Invalid instruction {inst}")
 
 
-def parse(in_filename: str, out_filename: str, symbol_table: Dict[str, str]) -> None:
-    # go through the entire program line by line
-    # - preprocess (remove whitespace, ignore empty lines) - done
-    # - determine if instruction is A/C/L (pseudo label instruction) - done
-    # - translate A inst to 0address - done
-    # - each time an A-instruction with symbol is encountered, see if it's already in the symbol table
-    # - if yes, read from it, else enter (new symbol : free RAM slot (starting at address 16)) to the table
-    # - translate C inst using the code module - done
-    # - write each translated line to out - done
+class Assembler:
+    """
+    Handles the assembly of a Hack assembly program into Hack binary machine code.
 
-    def get_c_parts(instruction: str) -> Dict[Optional[str], str]:
-        # The assembly C instruction in the format of dest=comp;jump where dest and jump are optional.
-        # at index: 0 is comp, 1 is dest, 2 is jump
-        parts = [None] * 3
+    Attributes:
+        symbol_table (Dict[str, str]): Maps symbols to binary addresses.
+        code (Code): Translates C-instruction fields to binary.
+        base_memory (int): Starting address for new A-instruction symbols in RAM, starting at 16.
 
-        # split left part : (maybe) jump
+    Methods:
+        parse(in_filename: str, out_filename: str) -> None
+        _handle_a_instruction(line: str, outfile: TextIO) -> None
+        _handle_c_instruction(line: str, outfile: TextIO) -> None
+        _get_c_parts(instruction: str) -> Dict[Optional[str], str]
+    """
+
+    def __init__(self, symbol_table: Dict[str, str]):
+        self.symbol_table = symbol_table
+        self.code = Code()
+        self.base_memory = 16
+
+    def parse(self, in_filename: str, out_filename: str) -> None:
+        """
+        Translates an assembly source file into binary code and writes the result to an output file.
+
+        Args:
+            in_filename (str): Path to the input assembly file.
+            out_filename (str): Path to the output file for binary code.
+        """
+        with open(in_filename, "r") as infile, open(out_filename, "w") as outfile:
+            for line in infile:
+                # preprocess
+                line = line.strip()  # remove whitespace
+                if line == "" or line[0] in "/(":
+                    continue  # ignore empty/ comment/ label line
+
+                if line[0] == "@":  # A-instruction
+                    self._handle_a_instruction(line, outfile)
+                else:  # C-instruction
+                    self._handle_c_instruction(line, outfile)
+
+    def _handle_a_instruction(self, line: str, outfile: TextIO) -> None:
+        """
+        Processes an A-instruction and writes its binary representation to the output file.
+
+        Each time a symbolic A-instruction is encountered, check if the symbol is already in the symbol table
+        If it is, read from it, else enter (new symbol : free RAM slot (starting at address 16)) to the table`
+        The binary representation is written to the output file.
+
+        Args:
+            line (str): The A-instruction line from the input file.
+            outfile (TextIO): The file object to write the binary output to.
+        """
+        address = line[1:]
+
+        try:  # valid integer address
+            out_line = "0" + to_binary(int(address))
+        except ValueError:  # symbolic address (variable (RAM) or label (ROM))
+            if address in self.symbol_table:
+                out_line = "0" + self.symbol_table[address]
+            else:
+                self.symbol_table[address] = to_binary(int(self.base_memory))
+                out_line = "0" + self.symbol_table[address]
+                self.base_memory += 1
+
+        outfile.write(out_line + "\n")
+
+    def _handle_c_instruction(self, line: str, outfile: TextIO) -> None:
+        """
+        Processes a C-instruction and writes its binary representation to the output file.
+
+        The method extracts the `comp`, `dest`, and `jump` fields and translates them using the `Code` class.
+        The binary representation is written to the output file.
+
+        Args:
+            line (str): The C-instruction line from the input file.
+            outfile (TextIO): The file object to write the binary output to.
+        """
+        # fields is in the format of [comp, dest, jump]
+        fields = self._get_c_fields(line)
+
+        comp = self.code.translate(fields[0], 0)
+        dest = self.code.translate(fields[1], 1)
+        jump = self.code.translate(fields[2], 2)
+
+        out_line = "111" + comp + dest + jump
+        outfile.write(out_line + "\n")
+
+    def _get_c_fields(self, instruction: str) -> List[str]:
+        """
+        Parses a C-instruction into its component fields: comp, dest, and jump.
+
+        Args:
+            instruction (str): The C-instruction in the format of 'dest=comp;jump'.
+
+        Returns:
+            List[str]: A list of ['comp', 'dest', 'jump'] fields of the C-instruction.
+        """
+        fields = [None] * 3
+
+        # split [left field, (maybe) jump]
         split1 = instruction.split(";")
         if len(split1) == 2:
-            parts[2] = split1[1]
+            fields[2] = split1[1]
 
-        # split (maybe) dest : comp
+        # split [(maybe) dest, comp]
         split2 = split1[0].split("=")
         if len(split2) == 2:
-            parts[1] = split2[0]
+            fields[1] = split2[0]
 
-        parts[0] = split2[-1]
-        return parts
+        fields[0] = split2[-1]
 
-    code = Code()
-
-    base_memory = 16
-    with open(in_filename, "r") as infile, open(out_filename, "w") as outfile:
-        for line in infile:
-            line = line.strip()
-            if line == "" or line[0] in "/(":
-                continue
-
-            if line[0] == "@":  # A instruction
-                address = line[1:]
-                try:
-                    out_line = "0" + to_binary(int(address))
-                except ValueError:
-                    if address in symbol_table:
-                        out_line = "0" + symbol_table[address]
-                    else:
-                        symbol_table[address] = to_binary(int(base_memory))
-                        out_line = "0" + symbol_table[address]
-                        base_memory += 1
-
-            else:  # C instruction
-                parts = get_c_parts(line)
-                comp = code.translate(parts[0], 0)
-                dest = code.translate(parts[1], 1)
-                jump = code.translate(parts[2], 2)
-                out_line = "111" + comp + dest + jump
-
-            outfile.write(out_line + "\n")
+        return fields
 
 
 def main():
@@ -233,7 +289,9 @@ def main():
 
     symbol_table = init_symbol_table()
     read_label_symbols(in_filename, symbol_table)
-    parse(in_filename, out_filename, symbol_table)
+
+    assembler = Assembler(symbol_table)
+    assembler.parse(in_filename, out_filename)
 
 
 if __name__ == "__main__":
