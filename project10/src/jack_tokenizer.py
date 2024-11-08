d791b1a4b30037ee1390383d0a5dabd70bbee864
keyxcode <keyxcode@gmail.com>
2024-11-09 06:50:15 +0700

    Implement tokenizer generator derivative functions

--- project10/src/jack_tokenizer.py
+++ project10/src/jack_tokenizer.py
@@ -1,12 +1,30 @@
 import re
-from typing import Union, Tuple, Iterator
-from typing import TextIO
+from collections import deque
+from typing import Union, Tuple, Iterator, TextIO
 
 class JackTokenizer:
     def __init__(self, infile: TextIO):
         self.infile = infile
+        self.buffer = deque()
+        self.token_generator = self.generate_token()
 
-    def get_token(self) -> Iterator[Tuple[Union[int, str], str]]:
+    def use_token(self) -> None:
+        # pop the first token in the buffer if there's any
+        # if buffer is empty, use the generator next
+        if self.buffer:
+            return self.buffer.popleft()
+        
+        return next(self.token_generator)
+
+    def buffer_token(self) -> None:
+        # call generator next and store it in the token queue
+        self.buffer.append(next(self.token_generator))
+
+    def peek_token(self, idx: int = 0) -> None:
+        # view token value in queue at idx, if there's any
+        return self.buffer[idx][1]
+
+    def generate_token(self) -> Iterator[Tuple[Union[int, str], str]]:
         keyword_pattern = r"\b(class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)\b"
         symbol_pattern = r"[{}()\[\].,;+\-*/&|<>=~]"
         integer_constant_pattern = r"\b(0|[1-9]\d{0,4})\b"
@@ -25,39 +43,12 @@ class JackTokenizer:
                 token_value = self.get_token_value(token_str, token_type)
                 yield (token_type, token_value)
 
-                # token_tag = ""            
-                # match token_type:
-                #     case "KEYWORD":
-                #         token_tag = "keyword"
-                #     case "SYMBOL":
-                #         token_tag = "symbol"
-                #     case "INT_CONST":
-                #         token_tag = "integerConstant"
-                #     case "STRING_CONST":
-                #         token_tag = "stringConstant"
-                #     case _:
-                #         token_tag = "identifier"
-                
-                
-                # print(token_value, token_type)
-                # outfile.write(f"<{token_tag}> ")
-                # outfile.write(f"{token_value}")
-                # outfile.write(f" </{token_tag}>\n")
-        
-        # outfile.write("</tokens>\n")
-
     def _strip_comments(self, code):
         single_line_pattern = r'//.*?$'
         multi_line_pattern = r'/\*.*?\*/'  
         pattern = f"{single_line_pattern}|{multi_line_pattern}"
         
         return re.sub(pattern, '', code, flags=re.DOTALL | re.MULTILINE).strip()
-
-    # def has_more_tokens(self) -> bool:
-    #     pass
-
-    # def advance(self) -> None:
-    #     pass
 
     def get_token_type(self, token: str) -> str:
         if token in ("class", "constructor", "function", "method", "field", "static", "var", "int", "char", "boolean", 
