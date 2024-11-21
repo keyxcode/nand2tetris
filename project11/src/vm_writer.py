c8a97a593d18c6115dae2290124c389aeec29a0c
keyxcode <keyxcode@gmail.com>
2024-11-21 19:33:35 +0700

    WIP: add vm writer boilerplate

--- project11/src/vm_writer.py
+++ project11/src/vm_writer.py
@@ -1,3 +1,38 @@
+from typing import TextIO, Literal
+
+_Segment = Literal["const", "arg", "local", "static", "this", "that", "pointer", "temp"]
+_Command = Literal["add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"]
+
 class VMWriter:
-    def __init__(self):
+    def __init__(self, outfile: TextIO):
+        self.outfile = outfile
+
+    def write_push(self, segment: _Segment, index: int) -> None:
         pass
+
+    def write_pop(self, segment: _Segment, index: int) -> None:
+        pass
+
+    def write_arithmetic(self, command: _Command) -> None:
+        pass
+
+    def write_label(self, label: str) -> None:
+        pass
+
+    def write_goto(self, label: str) -> None:
+        pass
+
+    def write_if(self, label: str) -> None:
+        pass
+
+    def write_call(self, name: str, num_args: int) -> None:
+        pass
+
+    def write_function(self, name: str, num_locals: int) -> None:
+        pass
+
+    def write_return(self) -> None:
+        pass
+
+    # def close(self) -> None:
+    #     pass
