f3ca4e94eea8973ddff7b9d36f6f98896cff3fa5
keyxcode <keyxcode@gmail.com>
2024-11-21 20:10:31 +0700

    WIP: fill out vm writer methods

--- project11/src/vm_writer.py
+++ project11/src/vm_writer.py
@@ -1,6 +1,6 @@
 from typing import TextIO, Literal
 
-_Segment = Literal["const", "arg", "local", "static", "this", "that", "pointer", "temp"]
+_Segment = Literal["constant", "argument", "local", "static", "this", "that", "pointer", "temp"]
 _Command = Literal["add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"]
 
 class VMWriter:
@@ -8,31 +8,31 @@ class VMWriter:
         self.outfile = outfile
 
     def write_push(self, segment: _Segment, index: int) -> None:
-        pass
+        self.outfile.write(f"push {segment} {index}")
 
     def write_pop(self, segment: _Segment, index: int) -> None:
-        pass
+        self.outfile.write(f"pop {segment} {index}")
 
     def write_arithmetic(self, command: _Command) -> None:
-        pass
+        self.outfile.write(command)
 
     def write_label(self, label: str) -> None:
-        pass
+        self.outfile.write(f"label {label}")
 
     def write_goto(self, label: str) -> None:
-        pass
+        self.outfile.write(f"goto {label}")
 
-    def write_if(self, label: str) -> None:
-        pass
-
-    def write_call(self, name: str, num_args: int) -> None:
-        pass
+    def write_if_goto(self, label: str) -> None:
+        self.outfile.write(f"if-goto {label}")
 
     def write_function(self, name: str, num_locals: int) -> None:
-        pass
+        self.outfile.write(f"function {name} {num_locals}")
+
+    def write_call(self, name: str, num_args: int) -> None:
+        self.outfile.write(f"call {name} {num_args}")
 
     def write_return(self) -> None:
-        pass
+        self.outfile.write("return")
 
     # def close(self) -> None:
     #     pass
